3ea5b37d4e9630c85105cf4fb6702795eb89dac4
Luiz Rioja <lulyrioja@gmail.com>
2022-12-07 17:35:59 -0300

    chore: refactor exercises

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,7 +1,7 @@
-var vel = 60.5
-console.log(`A velocidade do seu carro é ${vel} Km/h`)
+let vel = 60.5;
+console.log(`A velocidade do seu carro é ${vel} KM/h`);
 if (vel > 60) {
-    console.log('Você ultrapassou a velocidade permitida')
+  console.log("Você ultrapassou a velocidade permitida");
 } else {
-    console.log('Dirija sempre usando cinto de segurança!')
+  console.log("Dirija sempre usando cinto de segurança!");
 }
